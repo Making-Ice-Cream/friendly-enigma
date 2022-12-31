0b2fd577c0e822a2b99c3eb3b7bf22e4cfda5da3
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2022-12-31 10:08:32 +0530

    version 1.0.0

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA4klkL1c57gIbCS7kGGu5mlOPCNtL1Gsw",
+    authDomain: "utkansh2020-7858e.firebaseapp.com",
+    databaseURL: "https://utkansh2020-7858e.firebaseio.com",
+    projectId: "utkansh2020-7858e",
+    storageBucket: "utkansh2020-7858e.appspot.com",
+    messagingSenderId: "907622140597",
+    appId: "1:907622140597:web:60c069b5b2ce8e024accfc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
